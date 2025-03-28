df4653def5bc567277bfcc6f035f999b014827dd
swetasamantray <110470513+swetasamantray@users.noreply.github.com>
2025-03-28 09:39:31 +0530

    First push

---
+++ DummyFramework/src/test/java/contactTest/ToCreateContactTest.java
@@ -0,0 +1,11 @@
+package contactTest;
+
+public class ToCreateContactTest {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Contact created Successfully By Prakruti");
+
+	}
+
+}
